82deb053fa393bb65d0d466469b55f5da41e4019
Rishabh Singh <rishabhsinghsos123@gmail.com>
2024-08-11 08:55:12 +0530

    update

---
+++ Expressions/logicalop.js
@@ -0,0 +1,37 @@
+
+
+// Logical Operators
+
+var x = 5;
+var y= 6;
+
+// using AND operator
+console.log( x > 0 && y < 0);  // Gives False Value 
+
+
+//using OR operator
+console.log("Operator OR-- " + x<0 || y<0); // gives false
+console.log( x>0 || y<0); //Gives true Vale
+
+// usign NOT operator
+var isOpen =false;
+console.log(!isOpen); //gives opposite
+
+
+// ---------------------------------------------------------------------------------------------
+
+
+// Uranry Operator 
+
+console.log(+5);
+console.log(+"89");
+
+
+let m=0 , u=7 , h=0;
+
+ m= u--;//isme pehle m=7 store hoga fir minus hogaa u
+ h=--u;//isme pehle u minus hoga aur store hoga h me..
+
+
+ console.log("solution-m--" +m);
+ console.log("solution-h--" +h); 
--- Variables/My.js
+++ Variables/My.js
@@ -1,2 +1,20 @@
 
-console.log("Here the Rishabh Singh and Best Js course")
+console.log("Here the Rishabh Singh and Best Js course");
+
+var _myAge ="24years"; /*input taken */
+console.log(_myAge);/*output in terminal */
+
+var my_Age ="my age is 24 years";
+console.log(my_Age);
+
+
+// var 123age="here my age ";
+// console.log("123age")------------here is the wrong syntax to use it and give invalid and unexpected token;
+
+
+// var my@name = "rishabh";
+// colsole.log(my@name)----------Gives the syntax error--;
+
+
+var $adress ="vill dahan post saidput";
+console.log($adress);
